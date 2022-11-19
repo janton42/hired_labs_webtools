27fa2e92d59e51741f2376c69dc2daa2bc17e6ab
Jeff Stock <jantonstock@gmail.com>
2022-11-18 21:22:23 -0800

    working

--- resume_tailor/urls.py
+++ resume_tailor/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('profile/update/', views.update_profile, name='update_profile'),
     path('profile/settings/', views.settings, name='settings'),
     path('upload_resume/', views.upload_resume, name='upload_resume'),
-    path('parsed/<int:resume_id>', views.parsed_resume, name='parsed')
+    path('parsed/<int:resume_id>', views.parsed_resume, name='parsed'),
 ]
